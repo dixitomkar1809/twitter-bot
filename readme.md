93458c82a0514293ba87956bddc8c3574921b2e5
dixitomkar1809 <dixitomkar1809@gmail.com>
2019-04-01 23:37:12 -0500

    Created the streamer getting the tweets on the basis of the hashtag, looking forward to use socket programming to send the data from this streamer to plot the data on world map based on the sentiment of the tweet

--- readme.md
+++ readme.md
@@ -1,3 +1,15 @@
 Trying to create and efficient bot to do some analysis of tweets
 
 creds has my consumer_key, consumer_secret, access_token, access_token_secret
+
+Creating scraper to stream live tweets, since during my previous twitter sentimental analysis project the scraper was created and given to us in the course CS 6350 class.
+
+Since I had changed my laptop and the code was creating some errors so rather than just solving the error I preferred creating a similar streamer.
+
+I am planning on using some plotting libraries of python to plot the tweets on the world map based on the sentiment and the location.
+
+Let's see how far I get ahead with this.
+
+Feel free to ask me questions about the code.
+
+Thanks :D
---
+++ scraper.py
@@ -0,0 +1,49 @@
+# Author: Omkar Dixit
+# Source: http://docs.tweepy.org/en/v3.4.0/streaming_how_to.html
+
+import tweepy
+import json
+import requests
+import re
+from creds import *
+
+class MyStreamListener(tweepy.StreamListener):
+
+    # Here we just need the places where the location of the tweet is provided to make sure that we can do whatever analysis we want to do, based on the location.
+    def on_status(self, status):
+        if status.user.location is not None:
+            cleanTweet = self.clean_tweet(status.text)
+            googleMapsURL = "https://maps.googleapis.com/maps/api/geocode/json?address="+status.user.location+"&key="+googleMapsApi
+            results = requests.get(url=googleMapsURL)
+            data = results.json()
+            if len(data['results'])==0:
+                return
+            if len(data['results'][0]['address_components']) > 0:
+                for i in data['results'][0]['address_components']:
+                    lat = data['results'][0]['geometry']['location']['lat']
+                    lng = data['results'][0]['geometry']['location']['lng']
+                    dictItem = {'text': cleanTweet, 'location': status.user.location, 'coordinates': [lat, lng]}
+                    output = json.dumps(dictItem)
+                    print(output)
+                    output+="\n"
+    
+    def on_error(self, status_code):
+        if status_code==420:
+            return False
+        else:
+            print(status_code)
+
+    # Removes all the emojis and other punctuation to make the tweet clean and ready for sentiment analysis
+    def clean_tweet(self, tweet):
+        return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+
+if __name__=="__main__":
+    # Authorization
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+    
+    # Creating Listener
+    myStreamListener = MyStreamListener()
+    myStream = tweepy.Stream(auth = api.auth, listener = myStreamListener)
+    myStream.filter(track=['#TuesdayThoughts'])
